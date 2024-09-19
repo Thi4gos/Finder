509461261e075bb4fecffb50237dbd538e1da326
Thi4gos <ts214343@gmail.com>
2024-09-19 16:18:23 -0300

    pagesW

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'preferences',
+    loadComponent: () => import('./pages/preferences/preferences.page').then( m => m.PreferencesPage)
   },
 ];
